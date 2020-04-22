cbb17932e7badc246e7d554088c690695fab445f
Tal <tal.bny3@gmail.com>
2020-04-22 20:07:13 +0300

    removed the title bar and the notification bar

--- app/src/main/java/com/taban/partymode/MainActivity.kt
+++ app/src/main/java/com/taban/partymode/MainActivity.kt
@@ -2,11 +2,22 @@ package com.taban.partymode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //hide the title bar
+        getSupportActionBar()?.hide();
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main)
     }
 }
