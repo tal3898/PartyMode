7a24d1de4226c9db74a06cc2fd071befb0145fa6
tal <tal.bny3@gmail.com>
2020-04-24 20:42:41 +0300

    hide the navigation bar

--- app/src/main/java/com/taban/partymode/MainActivity.kt
+++ app/src/main/java/com/taban/partymode/MainActivity.kt
@@ -16,6 +16,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
         //hide the title bar
         getSupportActionBar()?.hide();
 
