798520d0039ee16433f986941947ac86138a798d
tal <tal.bny3@gmail.com>
2020-04-24 20:40:21 +0300

    made the screen stay open

--- app/src/main/java/com/taban/partymode/MainActivity.kt
+++ app/src/main/java/com/taban/partymode/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         //hide the title bar
         getSupportActionBar()?.hide();
